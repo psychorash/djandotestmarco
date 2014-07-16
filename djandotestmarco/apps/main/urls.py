840c09a3cf466c834bec645cc00674822e7c4ba7
marco <psycho.rash@gmail.com>
2014-07-16 14:26:26 -0500

    creacion de la vista main 2

--- djandotestmarco/apps/main/urls.py
+++ djandotestmarco/apps/main/urls.py
@@ -2,4 +2,5 @@ __author__ = 'marco'
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('djandotestmarco.apps.main.views',
-                       url(r'^$','index_view',name='url_index'))
+                       url(r'^$','index_view', name="url_index"),
+                       )
--- djandotestmarco/apps/main/views.py
+++ djandotestmarco/apps/main/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, render_to_response, RequestContext
 # Create your views here.
 
 def index_view(request):
-    return render_to_response('main/index.html',context_instance = RequestContext(request))
+    return render_to_response('main/index.html', context_instance = RequestContext(request))
