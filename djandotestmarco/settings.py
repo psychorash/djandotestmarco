7bcf9d010f47d9e049cba11f649b0eb7a25933cf
marco <psycho.rash@gmail.com>
2014-07-18 17:24:33 -0500

    creacion  y configuracion de media

--- djandotestmarco/settings.py
+++ djandotestmarco/settings.py
@@ -67,6 +67,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'djandotestmarco/media')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
