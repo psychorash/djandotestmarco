04c00ba4cf5d2e357df8241c8f4f0178eb43f7cd
marco <psycho.rash@gmail.com>
2014-07-17 21:38:23 -0500

    actualizando el settings

--- djandotestmarco/settings.py
+++ djandotestmarco/settings.py
@@ -1,25 +1,12 @@
-"""
-Django settings for djandotestmarco project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'plboh!#^v8@2bzqtxht^+sd^gz03)qyrd1u5$x#hgbzdf*0z+h'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 TEMPLATE_DEBUG = True
