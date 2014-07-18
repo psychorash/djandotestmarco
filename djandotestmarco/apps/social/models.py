6d35779aeaf8209591db34dd0a6fb3c7f2dfe40a
marco <psycho.rash@gmail.com>
2014-07-18 17:34:02 -0500

    creacion del model SocialNetwork

--- djandotestmarco/apps/social/models.py
+++ djandotestmarco/apps/social/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class SocialNetWork(models.Model):
+
+    def image_path(self, filename):
+       ruta ="SocialNetwork/%s/%s" % (self.settings_name, str(filename))
+       return ruta
+
+    name = models.CharField(max_length=300)
+    url = models.URLField(max_length=700)
+    icon = models.ImageField(upload_to=image_path)
+    status = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Red Social"
+        verbose_name_plural = "Redes Sociales"
